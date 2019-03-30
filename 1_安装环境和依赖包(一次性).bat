6a3761ba5b20eea899d26faf531f0791f6eba00c
xyqkent <xyqkent@163.com>
2019-03-30 14:24:48 +0800

    Update 1_安装环境和依赖包(一次性).bat

--- 1_安装环境和依赖包(一次性).bat
+++ 1_安装环境和依赖包(一次性).bat
@@ -1,14 +1,16 @@
 @ECHO OFF
-ECHO ����һЩ��Ҫ�ļ���/core/node_modules ...
+REM 声明采用UTF-8编码
+chcp 65001
+ECHO 下载一些必要文件到/core/node_modules ...
 ECHO.
-ECHO ����Ҳ���npmָ���ȷ���Ѿ���װNodeJS����������
+ECHO 如果找不到npm指令，请确保已经安装NodeJS并重启电脑
 ECHO.
-ECHO ���ķ�һЩʱ�䣬���App�����⣬��Ҳ����ɾ��/core/node_modules�ļ��в�����ִ�д�bat
+ECHO 这会耗费一些时间，如果App有问题，你也可以删了/core/node_modules文件夹并重新执行此bat
 ECHO.
 cd .\core
 call npm install
 ECHO.
-ECHO ��װnut��������
+ECHO 安装nut的依赖包
 ECHO.
 pip3 install colorama pyopenssl requests tqdm unidecode image bs4 urllib3 flask pyqt5 pyusb pyftpdlib
 ECHO.
